e6ea537f35bfb7bd47c8935a99b17b4fa85f64e4
w.w <151403603+mejulialeticia@users.noreply.github.com>
2024-05-31 16:55:32 +0000

    valores positivos e media

---
+++ Bee1064.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bee1064 {
+    public static void main(String args[]){
+        Scanner input= new Scanner(System.in);
+
+        int positivos = 0;
+        float soma = 0, media = 0;
+        float array[] = new float[6];
+
+        for(int i = 0; i<6; i++){
+            array[i] = input.nextFloat();
+            soma += array[i]; 
+            if(array[i] > 0){
+                positivos++;
+            }
+        }
+        if(positivos > 0){
+            media = soma/positivos;
+        }
+        
+        System.out.println(positivos + " valores positvos");
+        System.out.printf("%.1f%n", media);
+
+        input.close();
+    }
+}
